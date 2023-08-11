ed6ffedb0fa992939e124666ea06ba9a65e15801
MomoDaBro <momofqais@gmail.com>
2023-08-11 01:08:47 -0500

    Completed first three answer versions for questions ~ passed all test cases

--- src/main/kotlin/TwoSum.java
+++ src/main/kotlin/TwoSum.java
@@ -1,26 +1,29 @@
-import java.util.Arrays;
+import java.util.*;
 
-// Create two for loops
-// The outer loop will start at the first element and increment as the loop repeats
-// The inner loop will start at the second element and check if the element added to the element found in the outer loop equals to target
-// If it is equal return the indices, if not continue iterating
-// If the second loop reaches the end increment the first loop and continue checking if the elements add up.
+// Createa while loop that will terminate when target is found
+// Create an inner for loop that will iterate through the array
+//
+//
+//
+
 public class TwoSum {
 
     public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (i == j) continue;
-                if (nums[i] + nums[j] == target) return new int[]{i, j};
+            int num = nums[i];
+            if (numToIndex.containsKey(target - num)) {
+                return new int[]{numToIndex.get(target - num), i};
             }
+            numToIndex.put(nums[i], i);
         }
-        return new int[]{0,0};
+        throw new IllegalArgumentException();
     }
 
     public static void main(String[] arg) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        String expectedOutput = "[0,1]";
+        int[] nums = {3, 3};
+        int target = 6;
+        String expectedOutput = "[0, 1]";
         int[] actualOutput = twoSum(nums, target);
         System.out.println("Expected output is " + expectedOutput + " and actual output is " + Arrays.toString(actualOutput));
     }
